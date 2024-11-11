df3721d9cf7ae83c4f188fda0f4996a8e714343c
edogola4 <edogola4@gmail.com>
2024-11-11 19:53:40 +0300

    updated

--- script.js
+++ script.js
@@ -8,22 +8,5 @@ var typed = new Typed(".typing-text", {
 });
 
 // <!-- Loading counter overlay -->
-document.addEventListener("DOMContentLoaded", () => {
-    const counterElement = document.getElementById("counter");
-    const overlay = document.getElementById("loading-overlay");
-    const mainContent = document.getElementById("main-content");
-    let count = 0;
-  
-    const interval = setInterval(() => {
-      count += 10;
-      counterElement.textContent = count;
-  
-      if (count >= 100) {
-        clearInterval(interval);
-        overlay.classList.add("hidden");       // Hide the counter overlay
-        mainContent.classList.remove("hidden"); // Show main content
-      }
-    }, 500); // Update count every 0.5 seconds
-  });
-
-  
+// document.addEventListener("DOMContentLoaded", () => {
+   //
